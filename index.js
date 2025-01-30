a0dbf47a63f7cd439ecb68bf363ed083d14ab29c
Mikael Hirn <mikael.hirn@spottio.com>
2025-01-30 16:33:40 +0200

    Added express.static to serve the frontend.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 app.use(express.json())
 app.use(morgan(':method :url :response-time :data'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let contacts = [
 	    { 
